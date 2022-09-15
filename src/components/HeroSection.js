ab3aeedda4d1b3f8121afbf6a138d4accdc2574b
Rishav-narayan <78613789+Rishav-narayan@users.noreply.github.com>
2022-09-16 01:56:59 +0530

    supp

---
+++ src/components/HeroSection.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../App.css';
+import { Button } from './Button';
+import './HeroSection.css';
+
+function HeroSection() {
+  return (
+    <div className='hero-container'>
+      <video src='/videos/video-1.mp4' autoPlay loop muted />
+      <h1>ADVENTURE AWAITS</h1>
+      <p>What are you waiting for?</p>
+      <div className='hero-btns'>
+       
+          <Link
+                to='/DataCards'
+                className='one-links'
+              >
+           <Button
+          className='btns'
+          buttonStyle='btn--outline'
+          buttonSize='btn--large'
+        >
+          GET STARTED
+                  </Button>
+              </Link>
+      
+        <Button
+          className='btns'
+          buttonStyle='btn--primary'
+          buttonSize='btn--large'
+          onClick={console.log('hey')}
+        >
+          WATCH TRAILER <i className='far fa-play-circle' />
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default HeroSection;
---
+++ src/components/pages/DataCards.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Cards from "../Cards";
+
+function DataCards() {
+  return (
+    <Cards />
+  );
+}
+
+export default DataCards;
