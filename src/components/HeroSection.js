688050b95ad6a490b8176da99c4805fc48d7abef
Rishav <78613789+Web-Dev-hash@users.noreply.github.com>
2022-10-01 02:53:51 +0530

    one

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,7 +7,7 @@ import './HeroSection.css';
 function HeroSection() {
   return (
     <div className='hero-container'>
-      /*<video src='/videos/video-.mp4' autoPlay loop muted />*/
+    {/*<video src='/videos/video-.mp4' autoPlay loop muted />*/}
       <h1>ADVENTURE AWAITS</h1>
       <p>What are you waiting for?</p>
       <div className='hero-btns'>
